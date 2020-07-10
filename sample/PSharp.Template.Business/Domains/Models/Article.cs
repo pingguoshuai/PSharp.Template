25c33c8c787326e85e3d0d6ff487b155e2df7253
pingguoshuai <908008766@qq.com>
2020-07-10 14:48:32 +0800

    删除测试代码

--- sample/PSharp.Template.Business/Domains/Models/Article.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PSharp.Template.Business.Domains.Models {
-    /// <summary>
-    /// 文章
-    /// </summary>
-    public partial class Article {
-    }
-}
--- sample/PSharp.Template.Business/Domains/Models/Column.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PSharp.Template.Business.Domains.Models {
-    /// <summary>
-    /// 栏目
-    /// </summary>
-    public partial class Column {
-    }
-}
