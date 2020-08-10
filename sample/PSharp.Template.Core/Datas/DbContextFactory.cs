f4c542d64e052f44fe34646cbc71f0c8996dcf38
pingguoshuai <908008766@qq.com>
2020-08-10 17:41:19 +0800

    删除多余引用

--- sample/PSharp.Template.Core/Datas/DbContextFactory.cs
+++ sample/PSharp.Template.Core/Datas/DbContextFactory.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using PSharp.Template.Core.Datas.DbStrategy;
